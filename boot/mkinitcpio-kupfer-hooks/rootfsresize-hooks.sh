f40d82505fdcab61852720912d680ed37171cb33
Syboxez Blank <syboxez@gmail.com>
2022-05-31 00:09:20 -0500

    boot/mkinitcpio-kupfer-hooks: rewrite, use brute force partition scanning as fallback
    Co-authored-by: InsanePrawn <insane.prawny@gmail.com>

--- boot/mkinitcpio-kupfer-hooks/rootfsresize-hooks.sh
+++ boot/mkinitcpio-kupfer-hooks/rootfsresize-hooks.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/ash
 
 run_hook() {
-  if parted -s /dev/loop0 print free | tail -n2 | head -n1 | grep -qi "free space"; then
-    echo "Found unallocated space. Resizing..."
-    parted -s /dev/loop0 resizepart 2 100%
-    partprobe
-    e2fsck -fy /dev/loop0p2
-    resize2fs /dev/loop0p2
+  # rootfsdetect must be run first, or otherwise a root variable must be defined in the boot cmdline
+  if [ ! -n $root ]; then
+    echo "root variable undefined. Aborting..."
+    return 1
   fi
+
+  # Sets $LOOPDEV and $SUBPARTNUMBER
+  get_loopdev_partinfo $root
+
+  # Check if root is inside of a nested partition table and fall over to resizing fs if not
+  if [ -n $LOOPDEV ] && [ -n $SUBPARTNUMBER ] ; then
+    if parted -s "$LOOPDEV" print free | tail -n2 | head -n1 | grep -qi "free space"; then
+      echo "Found unallocated space on a subpartition. Resizing..."
+      parted -s "$LOOPDEV" resizepart "$SUBPARTNUMBER" 100%
+      partprobe
+    fi
+  fi
+
+  unset LOOPDEV
+  unset SUBPARTNUMBER
+
+  e2fsck -fy "$root"
+  # There is no need to check if filesystem needs to be resized since resize2fs already checks unless "-f" is specified
+  resize2fs "$root"
 }
